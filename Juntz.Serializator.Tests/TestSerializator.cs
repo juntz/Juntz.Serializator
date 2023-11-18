e7b1045b1778875039c762e0a7d617211e5d0ae6
Jun JUNG <j.jung.1394@gmail.com>
2023-11-18 19:08:42 +0900

    feat: add conversion function for byte array to object

--- Juntz.Serializator.Tests/TestSerializator.cs
+++ Juntz.Serializator.Tests/TestSerializator.cs
@@ -1,3 +1,5 @@
+using System.Buffers.Binary;
+
 namespace Juntz.Serializator.Tests
 {
     public class TestSerializator
@@ -16,6 +18,17 @@ namespace Juntz.Serializator.Tests
 
             var expected = new byte[] { 0x78, 0x56, 0x34, 0x12 };
             CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void FromBytes_ConvertSample_ReturnsExpected()
+        {
+            var bytes = new byte[] { 0x78, 0x56, 0x34, 0x12 };
+
+            var actual = Serializator.FromBytes<SampleDataUnit>(bytes);
+
+            var expected = 0x12345678;
+            Assert.That(actual.SampleField, Is.EqualTo(expected));
         }
     }
 }
--- Juntz.Serializator/Serializator.cs
+++ Juntz.Serializator/Serializator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
-using System.Text;
 
 namespace Juntz.Serializator
 {
@@ -10,10 +9,7 @@ namespace Juntz.Serializator
         public static byte[] ToBytes<T>(T value)
         {
             var type = typeof(T);
-            var layout = type.GetCustomAttribute<ByteLayoutAttribute>();
-            if (layout == null)
-                throw new ArgumentException("You must set the ByteLayout attribute to class or struct for serialization.");
-
+            var layout = GetLayout(type);
             var buffer = new byte[layout.Length];
             foreach (var property in type.GetProperties())
             {
@@ -33,6 +29,34 @@ namespace Juntz.Serializator
             return buffer;
         }
 
+        public static T FromBytes<T>(byte[] bytes) where T: new()
+        {
+            var result = new T();
+            var type = typeof(T);
+            foreach (var property in type.GetProperties())
+            {
+                var position = property.GetCustomAttribute<BytePositionAttribute>();
+                if (position == null)
+                {
+                    continue;
+                }
+
+                var valueBytes = bytes.Skip(position.Index).Take(position.Length).ToArray();
+                var value = ToValue(property.PropertyType, valueBytes);
+                property.SetValue(result, value);
+            }
+
+            return result;
+        }
+
+        private static ByteLayoutAttribute GetLayout(Type type)
+        {
+            var layout = type.GetCustomAttribute<ByteLayoutAttribute>();
+            if (layout == null)
+                throw new ArgumentException("You must set the ByteLayout attribute to class or struct for serialization.");
+            return layout;
+        }
+
         private static byte[] GetBytes(Type type, object value)
         {
             // TODO: Check endianness
@@ -42,6 +66,14 @@ namespace Juntz.Serializator
 
             var method = typeof(BitConverter).GetMethod("GetBytes", new [] { type });
             return (byte[])method.Invoke(null, new [] { value });
+        }
+
+        public static object ToValue(Type type, byte[] bytes)
+        {
+            var methodName = "To" + type.Name;
+            var argumentTypes = new [] { typeof(byte[]), typeof(int) };
+            var method = typeof(BitConverter).GetMethod(methodName, argumentTypes);
+            return method.Invoke(null, new object [] { bytes, 0 });
         }
     }
 }
